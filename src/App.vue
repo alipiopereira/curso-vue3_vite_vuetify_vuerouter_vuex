666cbcee076662c9d7d2d0660d3b985fa83ca3bd
alipiopereira <allipiopereira@gmail.com>
2021-03-14 07:22:31 -0300

    iniciando a brincadeira

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <div class="center"> 
+        <v-btn icon="mdi-minus" @click="state.counter--"></v-btn>
+        <h1 class="ma-3">{{state.counter}}</h1>
+        <v-btn icon="mdi-plus" @click="state.counter++"></v-btn>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { reactive } from "vue"
+
+const state = reactive({ counter: 0})
+</script>
+
+<style>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
